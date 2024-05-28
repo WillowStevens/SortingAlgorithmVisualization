3c3c8963fafcc760b9b0f0dbccdfc9d0adc7f1a4
Willow Stevens <William.Keith.Stevens@gmail.com>
2024-05-28 01:28:36 -0500

    Update script.js
    implemented Selection sort

--- script.js
+++ script.js
@@ -110,7 +110,23 @@ function insertionSort(array) {
     }
     return moves;
 }
-
+function selectionSort(array) {
+	const moves = [];
+	for(let i = 0; i<array.length;i++){
+		let minIndex = i;
+		for(let j = i+1; j < array.length;j++){
+			moves.push({indices: [minIndex, j], type: "comp"});
+			if(array[j] < array[minIndex]){
+				minIndex = j;
+			}
+		}
+		if(minIndex !== i){
+			[array[i], array[minIndex]] = [array[minIndex], array[i]];
+			moves.push({indices: [minIndex, i], type: "swap"});
+		}
+	}
+	return moves;
+}
 function showBars(move) {
     container.innerHTML = "";
     for (let i = 0; i < array.length; i++) {
